89c0a2bcb0a563a43c370f7b76d96c6c08092e67
Bihan-Banerjee <bihanbanerjee26@gmail.com>
2025-03-19 22:10:05 +0530

    added loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import React, { useState, useEffect } from "react";
 import WorldMap from "./components/WorldMap";
+import Loader from "./components/Loader";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000); 
+  }, []);
+
   return (
-    <div>
-      <WorldMap />
-    </div>
+    <>
+      {loading ? <Loader /> : <WorldMap />}
+    </>
   );
 }
 
